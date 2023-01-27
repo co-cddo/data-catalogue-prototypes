2ba633cc05bfddd43bab8cee6d0fcdb6c1a925d4
Lewis Nyman <hello@lewisnyman.co.uk>
2023-01-27 17:14:03 +0000

    GDX-288: Now we're cooking! Added data from the live catalogue

--- app/routes.js
+++ app/routes.js
@@ -6,16 +6,21 @@
 const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 const { promises: fs } = require("fs");
+let global = {};
 
 const init =  async function() {
+   global.organisations = await helpers.getData('./app/data/organisations.json');
    let resources = await helpers.getData('./app/data/resources.json');
+   const catalogue = await helpers.getData('./app/data/catalogue.json');
+   const mappedCatalogue = helpers.mapLiveSchemaToSpec(catalogue);
    const nhs = await helpers.getData('./app/data/nhs.json');
    const mappedNhs = helpers.mapLiveSchemaToSpec(nhs.apis, 'National Health Service');
-   resources = resources.concat(mappedNhs);
+   resources = resources.concat(mappedNhs, mappedCatalogue);
    
    // SPRINT 1 ROUTES
    router.get('/s1/find', function(req, res) {    
-    res.render("s1/find", { resources: resources });
+    const count = resources.length;
+    res.render("s1/find", { resources: resources, count: count });
    })
 }
 
@@ -30,16 +35,31 @@ const helpers = {
             console.log("e", error);
         }
     },
-    mapLiveSchemaToSpec(data, orgName) {
+    mapLiveSchemaToSpec(data) {
         return data.map(function(e) {
-            e.issuing_body = e.data.organisation;
-            e.title = e.data.name;
-            e.description = e.data.description;
-            // This is a cheat so I don't have to set up a proper org entity
-            e.issuing_body_readable = orgName;
+            if(e.data) {
+                e.title = e.data.name;
+                e.description = e.data.description;
+                e.issuing_body_readable = e.data.organisation;
+            }
+            else {
+                e.title = e.name;
+                e.description = e.description;
+                e.issuing_body_readable = helpers.getOrgTitle(e.provider);
+            }
             return e;
         }
         )
+    },
+    getOrgTitle(id) {
+        let name;
+        for (let i = 0; i < global.organisations.length; i++) {
+            if (global.organisations[i][id] == id) {
+                name = global.organisations[i][name];
+                break;
+            }
+        }
+        return name;
     }
 }
 
