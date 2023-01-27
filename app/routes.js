eab74b52606613d43366ccf5ec70271d5014a7d8
Lewis Nyman <hello@lewisnyman.co.uk>
2023-01-27 12:25:59 +0000

    GDX-288: Spit out some resource data

--- app/routes.js
+++ app/routes.js
@@ -5,5 +5,29 @@
 
 const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
+const { promises: fs } = require("fs");
 
-// Add your routes here
+const init =  async function() {
+   const resources = await helpers.getData('./app/data/resources.json');
+   console.log(resources);
+   
+   // SPRINT 1 ROUTES
+   router.get('/s1/find', function(req, res) {    
+    res.render("s1/find", { resources: resources });
+   })
+}
+
+
+const helpers = {
+    async getData(path) {
+        try {
+            const data = await fs.readFile(path, "utf-8");
+            return JSON.parse(data);
+    
+        } catch (error) {
+            console.log("e", error);
+        }
+    }
+}
+
+init();
