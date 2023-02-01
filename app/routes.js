6877b2585ee56cb721b4b28a2f6fce529efd6896
Lewis Nyman <hello@lewisnyman.co.uk>
2023-02-01 12:16:22 +0000

    Revert "Gdx-289: Filter by topics"

--- app/routes.js
+++ app/routes.js
@@ -13,24 +13,20 @@ let global = {};
 
 const init =  async function() {
    global.organisations = await helpers.getData('./app/data/organisations.json');
-   global.topics = await helpers.getData('./app/data/topics.json');
-   let resources = [];
+   let resources = await helpers.getData('./app/data/resources.json');
    const catalogue = await helpers.getData('./app/data/catalogue.json');
    const mappedCatalogue = helpers.mapLiveSchemaToSpec(catalogue);
    const nhs = await helpers.getData('./app/data/nhs.json');
-   const mappedNhs = await helpers.mapLiveSchemaToSpec(nhs.apis, 'nhs-digital', 'health');
-   resources =  await resources.concat(mappedNhs, mappedCatalogue);
+   const mappedNhs = helpers.mapLiveSchemaToSpec(nhs.apis, 'National Health Service');
+   resources = resources.concat(mappedNhs, mappedCatalogue);
 
-   global.index = searchSetup(resources);
-//    console.log(JSON.stringify(resources, 0, 2));
-//    console.log(JSON.stringify(global.index.data.items, 0, 2));
+   const index = searchSetup(resources);
    
    // SPRINT 1 ROUTES
    router.get('/s1/find', function(req, res) {  
     let items = resources;  
     let searchTerm;
     let appliedFilters = {};
-    let aggregations = global.index.data.aggregations;
     if (Object.keys(req.query).length !== 0) {
         if(req.query.q) {
             searchTerm = req.query.q;
@@ -43,35 +39,19 @@ const init =  async function() {
                 return true;
             })
         }
-        if(Array.isArray(req.query.topicFilters)) {
-            appliedFilters.topic = req.query.topicFilters.filter(function(e) {
-                if(e == '_unchecked') {
-                    return false;
-                }
-                return true;
-            })
-        }
         const results = search(searchTerm, appliedFilters);
         items = results.data.items;
-        aggregations = results.data.aggregations;
     }
 
     const filters = [
         {
-            title: 'Topics',
-            id: 'topicFilters',
-            items: helpers.generateFilterItems(global.topics, 'id', 'name', 'topicFilters', aggregations.topic),
-        },
-        {
             title: 'Organisations',
             id: 'organisationFilters',
-            items: helpers.generateFilterItems(global.organisations, 'id', 'name', 'organisationFilters', aggregations.issuing_body),
+            items: helpers.generateFilterItems(global.organisations, 'id', 'name', 'organisationFilters', req),
         }
     ]
     
     const count = items.length;
-    items = helpers.enrichTopics(items);
-    // console.log(JSON.stringify(items, 0, 2));
     res.render("s1/find", { resources: items, count: count, query: searchTerm, filters: filters });
    })
 }
@@ -85,16 +65,11 @@ const searchSetup = function(data) {
             }
         },
         aggregations: {
-            topic: {
-                title: 'Topics',
-                size: 30,
-                conjunction: false
-            },
             issuing_body: {
                 title: 'Organisations',
                 size: 30,
                 conjunction: false
-            },
+            }
         },
         searchableFields: ['title', 'description' ,'issuing_body_readable'],
     };
@@ -124,71 +99,32 @@ const helpers = {
             console.log("e", error);
         }
     },
-    mapLiveSchemaToSpec(data, issuing_body, topic) {
+    mapLiveSchemaToSpec(data) {
         return data.map(function(e) {
-            let n = {};
             if(e.data) {
-                n.title = e.data.name;
-                n.description = e.data.description;
-                n.issuing_body_readable = e.data.organisation;
-                n.issuing_body = issuing_body;
-                n.topic = helpers.splitTopics(topic);
+                e.title = e.data.name;
+                e.description = e.data.description;
+                e.issuing_body_readable = e.data.organisation;
+                e.issuing_body = 'nhs-digital';
             }
             else {
-                n.title = e.name;
-                n.description = e.description;
-                n.issuing_body = e.provider;
-                n.issuing_body_readable = helpers.getOrgTitle(e.provider);
-                if(e.topic) {
-                    n.topic = helpers.splitTopics(e.topic);
-                }
+                e.title = e.name;
+                e.description = e.description;
+                e.issuing_body = e.provider;
+                e.issuing_body_readable = helpers.getOrgTitle(e.provider);
             }
-            return n;
+            return e;
         }
         )
     },
-    splitTopics(string) {
-        const topics = [].concat(string.split(','));
-        return topics.map(function(e) {
-            const newTopic = global.topics.find(element => element.id == e);
-            try {
-                newTopic.id;
-            } catch (e) {
-                console.error('Topic does not match one of the pre-defined topics:' + e);
-            }
-            if(newTopic) {
-                return newTopic.id;
-            }
-            return "";
-        });
-    },
-    enrichTopics(items) {
-        items.forEach(function(item, index) {
-            const topics = item.topic.map(function(e) {
-                if(typeof e == 'object') {
-                    return e;
-                }
-                const newTopic = global.topics.find(topic => topic.id == e);
-                return newTopic;
-            });
-            items[index].topic = topics;
-        });
-        return items;
-    },
-    generateFilterItems(items, valueKey, textKey, groupId, aggregation) {
-        return aggregation.buckets.map(function(e) {
-            const ogFilterItem = items.find(item => item[valueKey] == e.key);
-            try {
-                ogFilterItem[valueKey];
-            } catch (e) {
-                console.error('Filter does not match one of the pre-defined options:' + e);
-            }
+    generateFilterItems(items, valueKey, textKey, groupId, req) {
+        const query = req.query;
+        return items.map(function(e) {
             let n = {};
-            n.value = ogFilterItem[valueKey];
-            n.text = ogFilterItem[textKey];
+            n.value = e[valueKey];
+            n.text = e[textKey];
             n.name = groupId;
-            console.log(e);
-            if(e.selected) {
+            if(query && query[groupId] && query[groupId].includes(n.value)) {
                 n.checked = 'checked'
             }
             return n;
