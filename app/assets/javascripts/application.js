441d66f5015afe80e58bca625946f9d6757ed774
Lewis Nyman <hello@lewisnyman.co.uk>
2023-02-01 15:01:40 +0000

    GDX-138: Tweaks to the wording and layout of the find page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,5 +4,22 @@
 //
 
 window.GOVUKPrototypeKit.documentReady(() => {
-  // Add JavaScript here
+  var $searchItem = $('.gem-c-search__item');
+  toggleFocus($searchItem);
+  $searchItem.on('change', function() {
+    toggleFocus(this);
+  });
 })
+
+function toggleFocus(elem) {
+    if(hasValue(elem)) {
+        $(elem).addClass('focus');
+    }
+    else {
+        $(elem).removeClass('focus');
+    }
+}
+
+function hasValue(elem) {
+    return $(elem).filter(function() { return $(this).val(); }).length > 0;
+}
