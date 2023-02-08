28b2eca4be34b16d0b83d7083e7ba0838a993a6b
Lewis Nyman <hello@lewisnyman.co.uk>
2023-02-08 12:38:18 +0000

    Added endpoint url

--- app/routes.js
+++ app/routes.js
@@ -179,6 +179,7 @@ const helpers = {
                     n.topic = helpers.splitTopics(e.topic);
                 }
             }
+            n.url = e.url;
             n.slug = n.title.toLowerCase().replaceAll(' ','-');
             return n;
         }
